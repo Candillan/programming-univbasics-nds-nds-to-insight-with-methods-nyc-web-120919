6d6f5f3f47a25a0623c2fd22995ebc12c7180a34
Calvin Baker <calvin.baker128@gmail.com>
2019-12-08 06:57:18 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,6 +13,7 @@ def directors_totals(nds)
     result[directors[:name]] = gross_for_director(directors)
     counter1 += 1
 	end
+	result
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
